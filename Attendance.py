a72df341d564c5613832504bd1728cfd67de83f8
nouressam043-web <nouressam043@gmail.com>
2026-08-20 02:09:10 +0000

    intial commit

--- Attendance.py
+++ Attendance.py
@@ -50,4 +50,5 @@ def history():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
